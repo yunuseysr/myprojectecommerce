a3a1b0841ad00494e8acfdaaec87de572704e714
Yunus Emre YAŞAR <yunuseysr@gmail.com>
2021-12-13 04:08:38 +0300

    update download file

--- Airflow/S3/downloadS3.py
+++ Airflow/S3/downloadS3.py
@@ -1,5 +1,12 @@
 import boto3
 
+# address to download the relevant data
 raw_path = 'C:/Users/yunus/OneDrive/Masaüstü/Brazillian ECommerce/myprojectecommerce/Data/'
+
+# Determination of the relevant structure as a customer through the library
 s3 = boto3.client('s3')
+# Example -> S3
+
+#Functionally, the bucket_name is the structure that downloads the data by taking the file name and extension to be downloaded.
 s3.download_file('myprojectecommerce', 'archive.zip', raw_path)
+
